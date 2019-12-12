1def7916c139567e541cf8c1fcd66881d4dd3f2e
bxf <1823146398@qq.com>
2019-12-12 15:42:56 +0800

    xx

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//后台模板
+Route::prefix('admin')->group(function () {
+   Route::get('index','Admin\IndexController@index'); //后台首页
+});
+
+
